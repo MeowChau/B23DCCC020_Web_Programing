f381d2959b318fda91bbec3f590d0fc6c1ea5843
Chau Nguyen <162424238+MeowChau@users.noreply.github.com>
2024-10-31 00:18:22 +0700

    Delete end_point directory

--- end_point/index.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-var sever = http.createServer(function(req, res) {
-    res.end("Hello World");
-});
-
-Server.listen(3000,function () {
-    console.log("Sever listening on http://localhost:3000")
-})
